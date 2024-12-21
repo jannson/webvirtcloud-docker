3553c5189340f4b3a3ee4fc92f13b94110cf7475
janson <janson@gmail.com>
2024-12-21 11:13:45 +0800

    build OK

---
+++ debian-docker/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#docker buildx build --platform linux/amd64 -t linkease/webvirtcloud:0.8.1 .
+docker buildx build --platform linux/amd64 -t linkease/webvirtcloud:0.8.5 -f DockerfileTest .
+#docker buildx build --platform linux/amd64 -t linkease/webvirtcloud:latest -f DockerfileTest .
+
