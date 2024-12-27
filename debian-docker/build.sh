5840c1e129a04201d6dc6cda2d4ab07c8f7a609b
janson <janson@gmail.com>
2024-12-27 18:51:49 +0800

    update ovmf

--- debian-docker/build.sh
+++ debian-docker/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 #docker buildx build --platform linux/amd64 -t linkease/webvirtcloud:0.8.1 .
-docker buildx build --platform linux/amd64 -t linkease/webvirtcloud:0.8.5 -f DockerfileTest .
+#docker buildx build --platform linux/amd64 -t linkease/webvirtcloud:0.8.5 -f DockerfileTest .
 #docker buildx build --platform linux/amd64 -t linkease/webvirtcloud:latest -f DockerfileTest .
+
+docker build -t linkease/webvirtcloud:0.8.6 -f DockerfileTest .
 
