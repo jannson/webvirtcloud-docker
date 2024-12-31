6d08ee1d157eae5af7e6e183c76eb7d626575dfe
janson <janson@gmail.com>
2024-12-31 22:55:33 +0800

    run docker

--- debian-docker/run-docker.sh
+++ debian-docker/run-docker.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 docker run --restart=unless-stopped -d \
-  --cgroupns=host \
-	--cap-add SYS_ADMIN \
-  --tmpfs /tmp \
-	--network host \
-  --privileged \
-	-v /sys/fs/cgroup:/sys/fs/cgroup \
-  -v /var/run:/var2/run \
-  -v /mnt/nvme/dbconfig:/srv/webvirtcloud/dbconfig \
-  -v /mnt:/mnt \
-	-e TZ=Asia/Shanghai \
-  --dns=172.17.0.1 \
-  --dns=223.5.5.5 \
-	--name webvirtcloud linkease/webvirttest:0.1
+        --cgroupns=host \
+        --cap-add SYS_ADMIN \
+        --tmpfs /tmp \
+        --network host \
+        --privileged \
+        -v /sys/fs/cgroup:/sys/fs/cgroup \
+        -v /projects:/projects \
+        -e TZ=Asia/Shanghai \
+        --dns=172.17.0.1 \
+        --dns=223.5.5.5 \
+        --name webvirtcloud linkease/webvirtcloud:latest
 
-#	--name istorePanel istorepanel:0.3
