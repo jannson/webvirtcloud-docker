eb1919ed5dee03abc91c6e70944d1c841b494144
janson <janson@gmail.com>
2025-01-01 14:30:27 +0800

    add bridge

--- pve-docker/tools/entrypoint.sh
+++ pve-docker/tools/entrypoint.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+if [ -S "/host/var/run/vmease/daemon.sock" ]; then
+  br=dsm-br
+  vnet1=dsm-int
+  vnet2=dsm-ext
+  curl -H "Content-Type: application/json" -X POST \
+    -d '{"br":"'$br'","vnet1":"'$vnet1'","vnet2":"'$vnet2'"}' \
+    --fail --max-time 15 --unix-socket /host/var/run/vmease/daemon.sock \
+    "http://localhost/api/vmease/create-br/"
+
+  if [ ! -z "$PVEIP" ]; then
+    ip addr flush dev eth0
+    ip addr add $PVEIP/$PVEMASK dev eth0
+    ip route add default via $PVEGATEWAY dev eth0
+  fi
+
+fi
+
 if [ ! -z "$root_password" ]; then
 	printf 'define the password,do config password %s\n' "$root_password"
 	echo "root:$root_password"|chpasswd
